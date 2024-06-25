d42939491d15040e1b2b46e475dbc2b56acf67c0
Oğuzhan Özenç <oguzhanozenc10@gmail.com>
2024-06-26 01:27:57 +0300

    updating header and navbar design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,43 @@
 import React from "react";
 import "./Header.css";
-
-import ME from "/me.png";
+import logo from "/logo-white.png";
 import { FaRegHandPeace } from "react-icons/fa6";
 import { PiProjectorScreenChartBold } from "react-icons/pi";
 
 export default function Header() {
   return (
-    <main id="home">
-      <section className="about--container" id="about">
-        <div className="about--container--img">
-          <img src={ME} alt="" />
-          <div className="about--container--buttons">
-            <a href="#contact" className="btn">
-              Say Hello! <FaRegHandPeace />
-            </a>
-            <a href="#portfolio" className="btn">
-              See my work! <PiProjectorScreenChartBold />
-            </a>
-          </div>
-        </div>
-
-        <div className="about--container--text">
-          <div>
-            <p id="heythere"> Hey there!👋 I'm</p>
-            <h1 id="myname">Oğuzhan Özenç</h1>
-            <p id="aboutmeparagraph">
+    <section className="header" id="home">
+      {" "}
+      <div className="header-img">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="header-container">
+        <div className="headercontainer-text">
+          <div className="headercontent">
+            <p id="heythere">Hey there!👋 I'm</p>
+            <p id="myname">Oguzhan Ozenc</p>
+            <p className="header-about">
               I'm a front-end developer who finds the journey from concept to
-              application incredibly rewarding. Personally, I've always been
-              fascinated by projects that tell stories, helping people share
-              their stories, and create impactful experiences. Software
-              development allows me to turn ideas into reality, which is why I
-              enjoy focusing on improving my skills in the front-end world.
-            </p>
-
-            <a href="/About" className="btn" id="moreaboutmebtn">
-              More about me&raquo;
-            </a>
+              application incredibly rewarding. I've always been fascinated by
+              projects that tell stories, helping people share their stories,
+              and create impactful experiences. Software development allows me
+              to turn ideas into reality, and I enjoy focusing on improving my
+              skills in the front-end world.{" "}
+              <a href="/About" className="moreaboutmebtn" id="moreaboutmebtn">
+                More about me&raquo;
+              </a>
+            </p>{" "}
+            <div className="headercontainer-buttons">
+              <a href="#contact" className="btn">
+                Say Hello! <FaRegHandPeace />
+              </a>
+              <a href="#portfolio" className="btn">
+                See my work! <PiProjectorScreenChartBold />
+              </a>
+            </div>
           </div>
         </div>
-      </section>
-    </main>
+      </div>
+    </section>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,145 +1,118 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
-import ME2 from "/me2.png";
-import {
-  PiMagicWand,
-  PiProjectorScreenChart,
-  PiPersonSimpleRunBold,
-  PiPaperPlaneTiltBold,
-} from "react-icons/pi";
-import { FiArrowUpRight } from "react-icons/fi";
-import { HiOutlineSparkles } from "react-icons/hi2";
-import { GiJourney } from "react-icons/gi";
-import { BsFileText } from "react-icons/bs";
-import { RiSpaceShipLine } from "react-icons/ri";
+import React, { useState, useEffect } from "react";
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import "./Navbar.css";
+import { BiSolidMessage } from "react-icons/bi";
 
 export default function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [activeItem, setActiveItem] = useState("Home");
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isHomePage = location.pathname === "/";
+  const [isScrolled, setIsScrolled] = useState(false);
 
-  function handleNavLinkClick(item) {
-    setActiveItem(item);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      const scrollThreshold = 100;
+      setIsScrolled(scrollTop > scrollThreshold);
+    };
+  }, []);
 
-    if (item === "Skills") {
-      const skillsSection = document.getElementById("skills");
-      if (skillsSection) {
-        skillsSection.scrollIntoView({ behavior: "smooth" });
-      }
+  const handleNavClick = (hash) => {
+    if (!isHomePage) {
+      navigate(`/#${hash}`);
     } else {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
+      const element = document.getElementById(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
     }
-
-    setMenuOpen(false); // Close the menu after clicking a link
-  }
-
-  function handleMenuToggle() {
-    setMenuOpen((prevMenuOpen) => !prevMenuOpen);
-  }
+  };
 
   return (
-    <div key={activeItem}>
-      <nav className="smallerscreensizenav">
-        {/* Hamburger Menu Icon */}
-        <div className="hamburger-icon--container">
-          <div className="topnavcontainer">
-            <div>
-              <button className="hamburger-icon" onClick={handleMenuToggle}>
-                <RiSpaceShipLine id="menuiconitself" />
-              </button>
-            </div>
-            <div>
-              <p className={`flip`}>{activeItem}</p>
-            </div>
-          </div>
+    <nav className={`navbar ${isScrolled ? "scrolled" : "transparent"}`}>
+      <div className="navbarlogo" key="navbarlogo">
+        {!isScrolled && (
+          <Link to="/" key="navbarlink">
+            <img src="/logo-white.png" alt="Logo" />
+          </Link>
+        )}
+      </div>
+      <div className="navbarmenu">
+        <div>
+          {isHomePage ? (
+            <a
+              href="#home"
+              className="nav-link"
+              onClick={(e) => {
+                e.preventDefault();
+                handleNavClick("home");
+              }}
+            >
+              Home
+            </a>
+          ) : (
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          )}
         </div>
-      </nav>
 
-      {/* Mobile Menu */}
-      {menuOpen && (
-        <div className="mobile-menu-open">
-          <div id="navbar--top-line" onClick={() => setMenuOpen(false)}>
-            <hr />
-          </div>
-
-          <div>
-            {/* Navbar List */}
-            <ul className="navbar--list">
-              {/* Avatar and Info */}
-              <div className="avatar">
-                <div className="navbar--icon">
-                  <img src={ME2} alt="Profile" className="navbar--image" />
-                </div>
-                <div className="navbar--infotext">
-                  <p>Oğuzhan Özenç</p>
-                  <small>Front-end Developer</small>
-                </div>
-              </div>
-
-              {/* Nav Links */}
-              <li className={activeItem === "Home" ? "active-nav" : ""}>
-                <NavLink to="/" onClick={() => handleNavLinkClick("Home")}>
-                  <HiOutlineSparkles /> Home
-                </NavLink>
-              </li>
-              <li className={activeItem === "Skills" ? "active-nav" : ""}>
-                <NavLink
-                  to="/#skills"
-                  onClick={() => handleNavLinkClick("Skills")}
-                >
-                  <PiMagicWand /> Skills
-                </NavLink>
-              </li>
-              <li className={activeItem === "Portfolio" ? "active-nav" : ""}>
-                <NavLink
-                  to="/Portfolio"
-                  onClick={() => handleNavLinkClick("Portfolio")}
-                >
-                  <PiProjectorScreenChart /> Portfolio
-                </NavLink>
-              </li>
-              <li className={activeItem === "About" ? "active-nav" : ""}>
-                <NavLink
-                  to="/About"
-                  onClick={() => handleNavLinkClick("About")}
-                >
-                  <PiPersonSimpleRunBold /> More About Me?
-                </NavLink>
-              </li>
-              <li className={activeItem === "Journey" ? "active-nav" : ""}>
-                <NavLink
-                  to="/Journey"
-                  onClick={() => handleNavLinkClick("Journey")}
-                >
-                  <GiJourney /> My Journey
-                </NavLink>
-              </li>
-              <li className={activeItem === "Contact" ? "active-nav" : ""}>
-                <NavLink
-                  to="/Contact"
-                  onClick={() => handleNavLinkClick("Contact")}
-                >
-                  <PiPaperPlaneTiltBold /> Contact
-                </NavLink>
-              </li>
-              <li className={activeItem === "Resume" ? "active-nav" : ""}>
-                <a
-                  href="/Oğuzhan Özenç_2024_Resume_en_website.pdf"
-                  onClick={() => handleNavLinkClick("Resume")}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <BsFileText /> Resume
-                  <FiArrowUpRight />
-                </a>
-              </li>
-            </ul>
-          </div>
+        <div>
+          <a
+            href="#portfolio"
+            className="nav-link"
+            onClick={(e) => {
+              e.preventDefault();
+              handleNavClick("portfolio");
+            }}
+          >
+            Projects
+          </a>
         </div>
-      )}
-    </div>
+
+        <div>
+          <a
+            href="#about"
+            className="nav-link"
+            onClick={(e) => {
+              e.preventDefault();
+              handleNavClick("about");
+            }}
+          >
+            About
+          </a>
+        </div>
+
+        <div>
+          <a
+            href="#journey"
+            className="nav-link"
+            onClick={(e) => {
+              e.preventDefault();
+              handleNavClick("journey");
+            }}
+          >
+            Journey
+          </a>
+        </div>
+
+        <div className="nav-link">
+          <Link to="/blog">Blog</Link>
+        </div>
+
+        <div id="contactbtn" className="navbutton nav--contactbtn">
+          <a
+            href="#contact"
+            className="nav-link"
+            onClick={(e) => {
+              e.preventDefault();
+              handleNavClick("contact");
+            }}
+          >
+            Contact <BiSolidMessage />
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 }
