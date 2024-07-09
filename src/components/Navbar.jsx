acc9b562f26baeb0e2af26b38889df918bee4d8f
Oğuzhan Özenç <oguzhanozenc10@gmail.com>
2024-07-09 16:15:25 +0300

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,18 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import "../css/Navbar.css";
 import { RiHomeSmile2Line } from "react-icons/ri";
 import { IoLayersOutline } from "react-icons/io5";
-import { TbMap2 } from "react-icons/tb";
-import { TbMessageChatbot } from "react-icons/tb";
+import { TbMap2, TbMessageChatbot } from "react-icons/tb";
 import { RiArrowRightLine } from "react-icons/ri";
 
-export default function Navbar() {
+const Navbar = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const isHomePage = location.pathname === "/";
+  const isHomePage = location.pathname === "/" && location.hash === "";
 
   const [isOpen, setIsOpen] = useState(false);
   const [hoveredIcon, setHoveredIcon] = useState(null);
 
-  const handleNavClick = (hash, e) => {
-    e.preventDefault();
+  const handleNavClick = (hash) => {
     if (!isHomePage) {
       navigate(`/#${hash}`);
     } else {
@@ -25,138 +23,80 @@ export default function Navbar() {
         element.scrollIntoView({ behavior: "smooth" });
       }
     }
-    setIsOpen(false);
   };
 
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
 
+  const renderNavLink = (to, icon, text) => {
+    const isActive = location.pathname === to || (isHomePage && to === "/");
+    const isHovered = hoveredIcon === to.replace("/", "");
+    const linkText = isOpen && <span className="nav-text">{text}</span>;
+
+    return (
+      <div
+        key={to}
+        className={`nav-link ${isActive ? "active" : ""}`}
+        onMouseEnter={() => setHoveredIcon(to.replace("/", ""))}
+        onMouseLeave={() => setHoveredIcon(null)}
+      >
+        {isHomePage ? (
+          <a
+            href={to === "/" ? "#home" : to}
+            className="nav-link"
+            onClick={(e) => handleNavClick(to, e)}
+          >
+            {icon}
+            {isOpen && <span className="nav-text">{text}</span>}
+          </a>
+        ) : (
+          <Link to={to} className="nav-link">
+            {icon}
+            {isOpen && <span className="nav-text">{text}</span>}
+          </Link>
+        )}
+        {!isOpen && <span className="hovered-text">{text}</span>}
+      </div>
+    );
+  };
+
   return (
     <nav className="navbar">
       <div className={`navbarmenu ${isOpen ? "open" : ""}`}>
         <div className="navbarlogo">
-          {isHomePage ? (
-            <a
-              href="#home"
-              className="nav-link"
-              onClick={(e) => handleNavClick("home", e)}
-            >
-              <img src="./logo.png" alt="" />
-            </a>
-          ) : (
-            <Link to="/" className="nav-link">
-              <img src="./logo.png" alt="" />
-            </Link>
-          )}
+          <Link to="/" className="nav-link">
+            <img src="./me2.png" alt="" />{" "}
+            {isOpen && (
+              <div className="title-container">
+                <p>Oğuzhan Özenç</p>
+                <small>Front-end Developer</small>
+              </div>
+            )}
+          </Link>
         </div>
         <div>
           <button className="navbarbtn" onClick={handleToggle}>
             <RiArrowRightLine />
           </button>
         </div>
-        <div
-          className={`nav-link ${location.pathname === "/" ? "active" : ""}`}
-          onMouseEnter={() => setHoveredIcon("home")}
-          onMouseLeave={() => setHoveredIcon(null)}
-        >
-          {isHomePage ? (
-            <a
-              href="#home"
-              className="nav-link"
-              onClick={(e) => handleNavClick("home", e)}
-            >
-              <RiHomeSmile2Line />
-              {hoveredIcon === "home" && !isOpen && (
-                <span className="nav-text">Home</span>
-              )}
-            </a>
-          ) : (
-            <Link to="/" className="nav-link">
-              <RiHomeSmile2Line />
-              {hoveredIcon === "home" && !isOpen && (
-                <span className="nav-text">Home</span>
-              )}
-            </Link>
-          )}
-        </div>
-        <div
-          className={`nav-link ${
-            location.pathname === "/journey" ? "active" : ""
-          }`}
-          onMouseEnter={() => setHoveredIcon("projects")}
-          onMouseLeave={() => setHoveredIcon(null)}
-        >
-          <a
-            href="#recentprojects"
-            className="nav-link"
-            onClick={(e) => handleNavClick("recentprojects", e)}
-          >
-            {isOpen ? (
-              <>
-                <IoLayersOutline /> Projects
-              </>
-            ) : (
-              <>
-                <IoLayersOutline />
-                {hoveredIcon === "projects" && !isOpen && (
-                  <span className="nav-text">Projects</span>
-                )}
-              </>
-            )}
-          </a>
-        </div>
-        <div
-          className={`nav-link ${
-            location.pathname === "/journey" ? "active" : ""
-          }`}
-          onMouseEnter={() => setHoveredIcon("journey")}
-          onMouseLeave={() => setHoveredIcon(null)}
-        >
-          <Link to="/journey" className="nav-link">
-            {isOpen ? (
-              <>
-                <TbMap2 />
-                Journey
-              </>
-            ) : (
-              <>
-                <TbMap2 />
-                {hoveredIcon === "journey" && !isOpen && (
-                  <span className="nav-text">Journey</span>
-                )}
-              </>
-            )}
-          </Link>
-        </div>
-        <div
-          className={`nav-link ${
-            location.pathname === "/journey" ? "active" : ""
-          }`}
-          onMouseEnter={() => setHoveredIcon("contact")}
-          onMouseLeave={() => setHoveredIcon(null)}
-        >
-          <a
-            href="#contact"
-            className="nav-link"
-            onClick={(e) => handleNavClick("contact", e)}
-          >
-            {isOpen ? (
-              <>
-                <TbMessageChatbot />
-                Contact
-              </>
-            ) : (
-              <>
-                <TbMessageChatbot />
-                {hoveredIcon === "contact" && !isOpen && (
-                  <span className="nav-text">Contact</span>
-                )}
-              </>
-            )}
-          </a>
+        <div className="navbarmenu-links">
+          <div className="home-icon">
+            {renderNavLink("/", <RiHomeSmile2Line />, "Home")}
+          </div>
+          {[
+            {
+              to: "/#recentprojects",
+              icon: <IoLayersOutline />,
+              text: "Projects",
+            },
+            { to: "/journey", icon: <TbMap2 />, text: "Journey" },
+            { to: "/#contact", icon: <TbMessageChatbot />, text: "Contact" },
+          ].map(({ to, icon, text }) => renderNavLink(to, icon, text))}
         </div>
       </div>
     </nav>
   );
-}
+};
+
+export default Navbar;
