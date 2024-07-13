b4b2ec328555e3ae30707a6617631c61edd31f00
Oğuzhan Özenç <oguzhanozenc10@gmail.com>
2024-07-14 01:26:58 +0300

    updating the letswork component and its style & animations

--- src/components/LetsWork.jsx
+++ src/components/LetsWork.jsx
@@ -1,13 +1,61 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../css/LetsWork.css";
 
+import { LuCopy, LuCopyCheck } from "react-icons/lu";
+import { TbMessageChatbot } from "react-icons/tb";
+
 export default function LetsWork() {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopyEmail = () => {
+    const email = "oguzhanozenc10@gmail.com";
+    navigator.clipboard
+      .writeText(email)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => {
+          setCopied(false);
+        }, 1000);
+      })
+      .catch((error) => {
+        console.error("Failed to copy: ", error);
+      });
+  };
+
   return (
     <div className="worktogether">
       <h2 className="sectiontitle">Let’s work together.</h2>
       <div className="aboutme-text">
-        <p>To create products that tells stories.</p>
+        <p>To create products that tell stories.</p>
       </div>
-      <a href="mailto:oguzhanozenc10@gmail.com">oguzhanozenc10@gmail.com</a>
+      <div className="worktogetherbtns">
+        {" "}
+        <div>
+          <Link
+            to="/contact"
+            className="button button-pulsating"
+            id="contactbtn"
+          >
+            Contact <TbMessageChatbot />
+          </Link>
+        </div>
+        <div>
+          <button id="copybtn" className="button" onClick={handleCopyEmail}>
+            <span className={`button-text ${copied ? "copied-animation" : ""}`}>
+              {copied ? (
+                <>
+                  <LuCopyCheck /> Copied
+                </>
+              ) : (
+                <>
+                  <LuCopy /> Copy Email
+                </>
+              )}
+            </span>
+          </button>{" "}
+        </div>
+      </div>
     </div>
   );
 }
