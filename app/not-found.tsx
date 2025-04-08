90f8eabc06b424ad6d5bc458ba5c9258449ec018
Oğuzhan Özenç <oguzhanozenc10@gmail.com>
2025-04-09 02:01:54 +0300

    addition of 404 not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="text-lg mt-4">
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
+}
