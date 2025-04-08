e5bed49983cb80e452d25b080127f9458f9da6de
Oğuzhan Özenç <oguzhanozenc10@gmail.com>
2025-04-09 02:31:20 +0300

    adjusting 404 page to deal it with later on

--- app/(main)/not-found.tsx
+++ app/(main)/not-found.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Button from "@/components/Button/Button";
 
 export default function NotFound() {
   return (
@@ -7,7 +8,9 @@ export default function NotFound() {
       <p className="text-lg mt-4">
         Sorry, the page you are looking for does not exist.
       </p>
-      <Link href="/">Return Home</Link>
+      <Button>
+        <Link href="/">Return Home</Link>
+      </Button>
     </div>
   );
 }
