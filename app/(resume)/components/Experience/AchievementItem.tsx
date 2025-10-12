e0aef90cf62f071a103f8660b8bf6a9c368450ec
Oğuzhan Özenç <oguzhanozenc10@gmail.com>
2025-10-12 23:29:25 +0300

    updating AchievementItem

--- app/(resume)/components/Experience/AchievementItem.tsx
+++ app/(resume)/components/Experience/AchievementItem.tsx
@@ -5,7 +5,6 @@ export default function AchievementItem({ children }: { children: ReactNode }) {
   return (
     <div className="text-sm text-[#444] leading-snug mb-1">
       <TextBlock>
-        <span className="font-semibold text-[#111827] mr-1">Achievements:</span>
         {children}
       </TextBlock>
     </div>
